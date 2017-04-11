f83bcc5f48961fefcb7a42c3415ebae086108e95
Ignacio Porris <igphonedev@gmail.com>
2017-04-11 15:20:10 -0700

    Initial Commit

---
+++ MaskieNF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaskieNF
+//
+//  Created by Ignacio Porris Gomez on 4/11/17.
+//  Copyright © 2017 Ignacio Porris Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
